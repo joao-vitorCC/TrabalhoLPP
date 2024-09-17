391e566301d5533254961b7afd47bc7cf71cee53
joao-vitorCC <joao.vitor2017cc@discente.ufg.br>
2024-09-16 21:58:07 -0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.io.File;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String nomeArquivo = args[0];
+		String ArquivoSaida = args[1];
+		Scanner sc = new Scanner(System.in);
+		//File f = new File(nomeArquivo);
+		//String str = sc.nextLine();
+		System.out.println(nomeArquivo);
+		sc.close();
+	}
+
+}
