3af8f3784b52009cff9bab2f44f255a4e0c5fd51
joao-vitorCC <joao.vitor2017cc@discente.ufg.br>
2024-09-18 08:15:24 -0300

    "Nome de entrada dos arquivos"

--- Test.java
+++ Test.java
@@ -6,19 +6,21 @@ public class Test {
 	//Teste
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		try{
-		    String nomeArquivo = args[0];
-		    String ArquivoSaida = args[1];
-		    System.out.println(nomeArquivo);	
-		}
-		catch(NullPointerException e){
-		     System.out.println("Escreva o nome do arquivo de entrada"); 	
-		     String nomeArquivo = sc.nextLine();
-		     System.out.println("Escreva o nome do arquivo de saida"); 
-		     String ArquivoSaida = sc.nextLine();	
+        String nomeArquivo;
+        String ArquivoSaida;
+
+		if(args.length == 2){
+		    nomeArquivo = args[0];
+            ArquivoSaida = args[1];
+        }else{
+            System.out.println("Escreva o nome do arquivo de entrada"); 	
+		    nomeArquivo = sc.nextLine();
+            System.out.println("Escreva o nome do arquivo de saida"); 	
+		     ArquivoSaida = sc.nextLine();
+        }    
+
+		    System.out.println(nomeArquivo + " " + ArquivoSaida);	
+            sc.close();
 		}
 		
-		sc.close();
 	}
-
-}
